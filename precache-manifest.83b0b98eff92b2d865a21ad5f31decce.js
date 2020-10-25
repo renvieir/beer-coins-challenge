73b9ed3e1614f05ec8cf75674d7e2dc093723fa2
douglas <douglas@melhorplano.net>
2020-10-25 18:28:11 -0300

    Updates

---
+++ precache-manifest.83b0b98eff92b2d865a21ad5f31decce.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909013dacc5a7b71c44e5bcde33f27e4",
+    "url": "/beer-coins-challenge/index.html"
+  },
+  {
+    "revision": "e88f5d41b4cbf6d08b7a",
+    "url": "/beer-coins-challenge/static/css/2.9db35e51.chunk.css"
+  },
+  {
+    "revision": "9ddae375e115b12b322d",
+    "url": "/beer-coins-challenge/static/css/main.7cf6df80.chunk.css"
+  },
+  {
+    "revision": "e88f5d41b4cbf6d08b7a",
+    "url": "/beer-coins-challenge/static/js/2.b9d3bb79.chunk.js"
+  },
+  {
+    "revision": "08f88c415865709a5c804f3e744b1d9d",
+    "url": "/beer-coins-challenge/static/js/2.b9d3bb79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ddae375e115b12b322d",
+    "url": "/beer-coins-challenge/static/js/main.9cf6c121.chunk.js"
+  },
+  {
+    "revision": "e6a80715d1749e575eae",
+    "url": "/beer-coins-challenge/static/js/runtime-main.3915087d.js"
+  },
+  {
+    "revision": "36f0f834a0442be913cbd7bc0720030a",
+    "url": "/beer-coins-challenge/static/media/404.36f0f834.svg"
+  },
+  {
+    "revision": "e0b4c0eecfeeda3f6b2a2bbc25eb88d6",
+    "url": "/beer-coins-challenge/static/media/admin.e0b4c0ee.svg"
+  },
+  {
+    "revision": "6ba2a92b28b5ea0575478d6fa79af389",
+    "url": "/beer-coins-challenge/static/media/beer-image.6ba2a92b.jpg"
+  },
+  {
+    "revision": "e20b6b8ea6c6e0249659204ba44c9d4b",
+    "url": "/beer-coins-challenge/static/media/beer-tech-logo.e20b6b8e.png"
+  },
+  {
+    "revision": "1cad8013a069d11c2786ce0ed49e7fb0",
+    "url": "/beer-coins-challenge/static/media/client.1cad8013.svg"
+  },
+  {
+    "revision": "7a809f5c121a0b2b4726ee9553669ac6",
+    "url": "/beer-coins-challenge/static/media/empty.7a809f5c.svg"
+  },
+  {
+    "revision": "b0c2dbc8ea3be5db2d30838d51ff75ca",
+    "url": "/beer-coins-challenge/static/media/logo.b0c2dbc8.svg"
+  },
+  {
+    "revision": "4acaec57bbaffa8cfd8d087fd1547791",
+    "url": "/beer-coins-challenge/static/media/operations.4acaec57.svg"
+  },
+  {
+    "revision": "8c2c7ec72568e1aae3ce5aa5dfe01c7f",
+    "url": "/beer-coins-challenge/static/media/receipt.8c2c7ec7.svg"
+  },
+  {
+    "revision": "19c1477d27a16dedf8f6addd5721409f",
+    "url": "/beer-coins-challenge/static/media/rewards.19c1477d.svg"
+  },
+  {
+    "revision": "91f38c68564d80c46654e96a838981d9",
+    "url": "/beer-coins-challenge/static/media/tap-beer.91f38c68.mp4"
+  },
+  {
+    "revision": "390b52191bc53079dd85b87b027f20f1",
+    "url": "/beer-coins-challenge/static/media/transfer.390b5219.svg"
+  }
+]);
